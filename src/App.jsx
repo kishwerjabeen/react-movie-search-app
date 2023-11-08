7d2efd4adefa335c030f8a6a8113d47e9c800cd4
Kishwar Jabeen <kishwerjabeen90@gmail.com>
2023-11-09 03:08:34 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import SearchBar from './components/SearchBar'
+import MovieCard from './components/MovieCard'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <SearchBar/>
+      <MovieCard/>
+    </div>
+  )
+}
+
+export default App
