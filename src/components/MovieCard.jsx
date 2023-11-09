14f526a20f0774f20b0719983b452a4ca5d8367f
Kishwar Jabeen <kishwerjabeen90@gmail.com>
2023-11-09 14:17:24 +0500

    design movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,56 @@ import React from 'react'
 
 const MovieCard = () => {
   return (
-    <div>MovieCard</div>
+    <div>
+      <div className='flex flex-wrap px-4 lg:px-10'>
+        <div className='p-2 md:w-1/4 w-full'>
+          <div className='bg-[#FF7474] p-3 rounded-2xl shadow-lg hover:-translate-y-1 border-2 border-gray-600'>
+            <img className='rounded-lg w-full mb-2' src="https://dummyimage.com/720x400" alt="" />
+            <h2 className='text-xl text-white font-bold'>This is title</h2>
+            <h2 className='text-lg text-white mb-2'>desc</h2>
+          </div>
+        </div>
+
+
+        <div className='p-2 md:w-1/4 w-full'>
+          <div className='bg-[#FF7474] p-3 rounded-2xl shadow-lg hover:-translate-y-1 border-2 border-gray-600'>
+            <img className='rounded-lg w-full mb-2' src="https://dummyimage.com/720x400" alt="" />
+            <h2 className='text-xl text-white font-bold'>This is title</h2>
+            <h2 className='text-lg text-white mb-2'>desc</h2>
+          </div>
+        </div>
+
+
+        <div className='p-2 md:w-1/4 w-full'>
+          <div className='bg-[#FF7474] p-3 rounded-2xl shadow-lg hover:-translate-y-1 border-2 border-gray-600'>
+            <img className='rounded-lg w-full mb-2' src="https://dummyimage.com/720x400" alt="" />
+            <h2 className='text-xl text-white font-bold'>This is title</h2>
+            <h2 className='text-lg text-white mb-2'>desc</h2>
+          </div>
+        </div>
+
+        <div className='p-2 md:w-1/4 w-full'>
+          <div className='bg-[#FF7474] p-3 rounded-2xl shadow-lg hover:-translate-y-1 border-2 border-gray-600'>
+            <img className='rounded-lg w-full mb-2' src="https://dummyimage.com/720x400" alt="" />
+            <h2 className='text-xl text-white font-bold'>This is title</h2>
+            <h2 className='text-lg text-white mb-2'>desc</h2>
+          </div>
+        </div>
+
+        <div className='p-2 md:w-1/4 w-full'>
+          <div className='bg-[#FF7474] p-3 rounded-2xl shadow-lg hover:-translate-y-1 border-2 border-gray-600'>
+            <img className='rounded-lg w-full mb-2' src="https://dummyimage.com/720x400" alt="" />
+            <h2 className='text-xl text-white font-bold'>This is title</h2>
+            <h2 className='text-lg text-white mb-2'>desc</h2>
+          </div>
+        </div>
+
+
+      </div>
+
+      
+
+    </div>
   )
 }
 
